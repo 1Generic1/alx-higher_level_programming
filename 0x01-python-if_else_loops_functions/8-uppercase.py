e4fa5210c5642bb23eaa9ebc0eb8b149a721c8f1
kelvin <kelvin4stunt@gmail.com>
2023-08-10 08:11:05 +0100

    else

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+def uppercase(s):
+    for char in s:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            uppercase_char = chr(ascii_value - 32)
+            print(uppercase_char, end="")
+        else:
+            print(char, end="")
+    print()
