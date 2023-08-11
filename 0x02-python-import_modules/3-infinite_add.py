03d5bf6e06e0dabfcbdd9f987e41440d3539b313
kelvin <kelvin4stunt@gmail.com>
2023-08-11 09:18:13 +0100

    import & modules

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,6 @@
 import sys
 
 if __name__ == "__main__":
-    result = 0
-    for arg in sys.argv:
-        if arg != sys.argv[0]:
-            result += int(arg)
-            print(result)
+    arguments = sys.argv[1:]
+    total = sum(map(int, arguments))
+    print(total)
