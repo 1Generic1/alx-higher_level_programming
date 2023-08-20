8e7252ba1f59d3dee02e599a47f580d3dd73106c
kelvin <kelvin4stunt@gmail.com>
2023-08-20 09:32:50 +0100

    data structures

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/bin/usr/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary{}
+    for key, value in a_dictionary.item():
+        new_value = value * 2
+        new_dictionary[key] = new_value
+    return new_dictionary
