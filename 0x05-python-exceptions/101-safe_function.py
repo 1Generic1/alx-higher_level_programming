3b61853159d651c48f70fd9ee166fd0bbe272edb
kelvin <kelvin4stunt@gmail.com>
2023-08-29 17:19:23 +0100

    exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,9 @@
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return None
