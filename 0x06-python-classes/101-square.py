fc3ef8e59c3f8ad4a9fb8801ec976a3e4a1e7bfb
kelvin <kelvin4stunt@gmail.com>
2023-09-02 03:00:56 +0100

    update

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+""" a class Square that defines a square """
+
+
+class Square:
+    """ A class Square that defines a square.
+
+    Attributes:
+     __size (int): The size of the square.
+        __position (tuple): The position of the square.
+    """
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) and len(value) !=2:
+            raise TypeError("position must be a tuple of 2 positive integer")
+        if not all(isinstance(num, int) and num >= 0 for num in value):
+            raise TypeError("position must be a tuple of 2 positive integer")
+        self.__position = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+            return
+        square_str = ""
+        for _ in range(self.__position[1]):
+            square_str += "\n"
+        for _ in range(self.__size):
+            square_str += " " * self.__position[0] + "#" * self.__size + "\n"
+        return square_str
+    def __str__(self):
+        return self.my_print()
+
