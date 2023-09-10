bdd16ec38d69f5c013be1b8c8c0d89324bf24d48
kelvin <kelvin4stunt@gmail.com>
2023-09-10 20:09:01 +0100

    Everything is object

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -4,4 +4,5 @@
 
 def magic_string(n=1):
     magic_string.counter = getattr(magic_string, "counter", 0) + 1
-    return "BestSchool" * magic_string.counter
+    result = "BestSchool, " * magic_string.counter
+    return result.rstrip(", ")
