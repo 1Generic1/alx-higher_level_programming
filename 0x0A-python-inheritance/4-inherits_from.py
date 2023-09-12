11ba020ed8dac6a82a633c9d150b5bea2a50efce
kelvin <kelvin4stunt@gmail.com>
2023-09-12 06:09:45 +0100

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ a function that returns True if the object is an
-    instance of a class that inherited (directly or indirectly) from the specified class ; otherwise    False.
+    instance of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise    False.
 """
 
 
