14a5cfb5f3028f420d1aaa3c35c6063f851c2f53
kelvin <kelvin4stunt@gmail.com>
2023-09-12 23:26:51 +0100

    input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """ function that reads a text file (UTF8) and prints it to stdout """
 
+
 def read_file(filename=""):
-    """ reads file """
+    """ reads file
+    Args:
+    filename: filename
+    """
     try:
         with open(filename, mode="r", encoding="utf-8") as file:
             print(file.read(), end="")
