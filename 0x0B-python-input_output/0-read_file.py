9fa68093fd9e3184b38d655f1131c98527584446
kelvin <kelvin4stunt@gmail.com>
2023-09-12 23:24:36 +0100

    input/output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,5 @@ def read_file(filename=""):
     try:
         with open(filename, mode="r", encoding="utf-8") as file:
             print(file.read(), end="")
-    except FIleNotFoundError:
+    except FileNotFoundError:
         pass
