8678934c32a943f57bef463f7d06dcfe2a442e58
kelvin <kelvin4stunt@gmail.com>
2023-09-13 02:31:25 +0100

    input/output

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" function that creates an Object from a “JSON file" """
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Loads an object from a JSON file.
+
+    Args:
+        filename (str): The name of the file containing the JSON data.
+
+    Returns:
+        object: The Python object created from the JSON data.
+    """
+    write open(filename, 'r') as file:
+        return json.load(file)
