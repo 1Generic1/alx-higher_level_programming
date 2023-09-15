249ce9539c4b0ea1902621f10a5d84245cefdba3
kelvin <kelvin4stunt@gmail.com>
2023-09-15 04:05:13 +0100

    input/output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -12,6 +12,6 @@ def class_to_json(obj):
         raise TypeError("Input is not an instance of a class")
     serialized_data = {}
     for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool))::
+        if isinstance(value, (list, dict, str, int, bool)):
             serialized_data[key] = value
     return serialized_data
