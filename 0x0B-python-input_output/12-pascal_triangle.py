c03fba757790e80444cf4a7b504e260323965407
kelvin <kelvin4stunt@gmail.com>
2023-09-15 05:05:53 +0100

    input/output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,10 +9,11 @@ def pascal_triangle(n):
     triangle = [[1]]
     for i in range(1, n):
         previous_row = triangle[-1]
-        new_rpw = [1]
+        new_row = [1]
         for j in range(1, i):
             new_element = previous_row[j - 1] + previous_row[j]
             new_row.append(new_element)
 
         new_row.append(1)  # The last element is always 1
         triangle.append(new_row)
+    return triangle
