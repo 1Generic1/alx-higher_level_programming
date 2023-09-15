97d1501fa8273dfbd198d6a2f840eee6cf1134ee
kelvin <kelvin4stunt@gmail.com>
2023-09-15 04:53:18 +0100

    input/output

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" class Student that defines a student """
+
+
+class Student:
+    """ defines student """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_nmae = last_name
+        self.age = age
+    
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        json_data = {}
+        for attr in attrs:
+            if hasattr(self, attr):
+                json_data[attr] = getattr(self, attr)
+        return json_data
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
