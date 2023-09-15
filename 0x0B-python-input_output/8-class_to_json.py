843d9804e71cb4bfe0b0a39bb7d37b8c7c61198e
kelvin <kelvin4stunt@gmail.com>
2023-09-15 04:03:27 +0100

    input/output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" function that returns the dictionary description
+    with simple data structure (list, dictionary, string,
+    integer and boolean) for JSON serialization of an object
+"""
+
+
+def class_to_json(obj):
+    """ function that returns the dictionary """
+
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("Input is not an instance of a class")
+    serialized_data = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool))::
+            serialized_data[key] = value
+    return serialized_data
