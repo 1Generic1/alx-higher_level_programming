377f5957899e73c1f1b19eecf438c03f8fc9727b
kelvin <kelvin4stunt@gmail.com>
2023-09-19 23:14:46 +0100

    python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -77,12 +77,14 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
+        """Print the Rectangle using the `#` character."""
         for _ in range(self.__y):
             print()
         for _ in range(self.__height):
             print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
         return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - "
                 f"{self.width}/{self.height}")
 
