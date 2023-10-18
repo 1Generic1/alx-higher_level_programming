2e7986c9093863fc320bbd80b3f5563b8888609f
kelvin <kelvin4stunt@gmail.com>
2023-10-18 23:00:51 +0100

    sql

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Check if the database 'hbtn_0c_0' exists. If it does, the following line is a no-op.
+-- If it doesn't exist, the script creates the 'hbtn_0c_0' database.
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
