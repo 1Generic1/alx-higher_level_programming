781c7d33189a12a5cdddb5b305bc6047d6c09b90
kelvin <kelvin4stunt@gmail.com>
2023-10-20 15:42:06 +0100

    sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,10 @@
+-- script that displays the top 3 of cities temperature
+-- during July and August ordered by temperature (descending)
+
+USE hbtn_0c_0;
+SELECT city, AVG(temperature) AS avg_temperature
+FROM Temperatures
+WHERE month IN (7, 8)
+GROUP BY city
+ORDER BY avg_temperature DESC
+LIMIT 3;
