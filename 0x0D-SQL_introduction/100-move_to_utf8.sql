cfc18598758faf51f601b097c256bae5d8f6573f
kelvin <kelvin4stunt@gmail.com>
2023-10-20 15:13:51 +0100

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -2,15 +2,10 @@
 -- the name field in the first_table to UTF-8
 -- (utf8mb4 with collation utf8mb4_unicode_ci)
 USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0
-	CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
-
+-- Modify the table first_table to use the utf8mb4 character set and utf8mb4_unicode_ci collation
 ALTER TABLE first_table
-	CONVERT TO CHARACTER SET utf8mb4
-	COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+-- Modify the name column in first_table to use the utf8mb4 character set and utf8mb4_unicode_ci collation
 ALTER TABLE first_table
-  MODIFY COLUMN name VARCHAR(256)
-  CHARACTER SET utf8mb4
-  COLLATE utf8mb4_unicode_ci;
+MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
