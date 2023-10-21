d76956ebb7168c56ed990d74ffda08b819c861e1
kelvin <kelvin4stunt@gmail.com>
2023-10-22 00:05:32 +0100

    sql

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+--  script that creates the table force_name on your MySQL server.
+
+CREATE TABLE IF NOT EXISTS force_name (
+	-- define each column content
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL
+	);
