d4b270bc0bf0778aa884ab240ab336cab050ed60
kelvin <kelvin4stunt@gmail.com>
2023-10-22 01:32:23 +0100

    sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,7 +3,7 @@
 
 USE hbtn_0d_usa;
 
-SELECT cities.id, cities.name
+SELECT id, name
 FROM cities
 WHERE cities.state_id = (SELECT id FROM states WHERE name = 'california')
-ORDER BY cities.id;
+ORDER BY id;
