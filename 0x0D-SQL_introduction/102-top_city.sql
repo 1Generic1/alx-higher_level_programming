8f3add3940500d4cdddd6f60932afa0fc536dc92
kelvin <kelvin4stunt@gmail.com>
2023-10-24 15:11:35 +0100

    sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- during July and August ordered by temperature (descending)
 
 USE hbtn_0c_0;
-SELECT city, AVG(value) AS avg_temperature
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 WHERE month IN (7, 8)
 GROUP BY city
