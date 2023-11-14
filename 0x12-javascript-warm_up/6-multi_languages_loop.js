d156aa55906481f0e95a763876cc255ef146fded
kelvin <kelvin4stunt@gmail.com>
2023-11-14 04:02:07 +0100

    java

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
-if (!isNaN(x)) {
-  for (let i = 0; i < x; i++) {
-    console.log('C is fun');
-  }
-} else {
-  console.log('Missing number of occurrences');
+
+const lines = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
 }
