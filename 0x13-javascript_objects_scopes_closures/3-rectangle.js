a9baa2cf195c33c05905a4f782bfc63ca02e5b28
kelvin <kelvin4stunt@gmail.com>
2023-11-15 04:50:04 +0100

    javascript

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      Object.defineProperty(this, 'width', { value: undefined, enumerable: false });
+      Object.defineProperty(this, 'width', { value: undefined, enumerable: false });
+    }
+  }
+
+  print () {
+    if (this.width !== undefined && this.height !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Rectangle;
