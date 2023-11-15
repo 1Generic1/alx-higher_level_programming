25b747651d7f316de79063bc98489ea9afd05712
kelvin <kelvin4stunt@gmail.com>
2023-11-15 06:04:42 +0100

    javascript

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,7 +3,7 @@
 const Square = require('./5-square');
 
 class ExtendedSquare extends Square {
-  charPrint(c) {
+  charPrint (c) {
     const character = c || 'X';
     for (let i = 0; i < this.height; i++) {
       console.log(character.repeat(this.width));
