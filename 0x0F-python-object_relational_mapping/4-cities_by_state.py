b4c6cc122ed0425021ec20d53c661d787bdad8f1
kelvin <kelvin4stunt@gmail.com>
2023-11-20 10:31:46 +0100

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,27 +8,18 @@ import MySQLdb
 import sys
 
 
-def list_cities(username, password, database):
-    """
-    Connects to the MySQL server and retrieves values
-    from the states table where the name matches the provided state name.
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database> <state_name>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-        search_name (str): State name to search for.
-
-    Returns:
-        None
-    """
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd="Mysqlpassword1$$",
-        db=database
-        )
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=username, passwd="Mysqlpassword1$$",
+                         db=database)
     cursor = db.cursor()
     query = (
         "SELECT cities.id, cities.name, states.name FROM "
@@ -36,19 +27,8 @@ def list_cities(username, password, database):
         "ORDER BY id ASC"
         )
     cursor.execute(query)
-    cities = cursor.fetchall()
-    for city in cities:
-        print(city)
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
     cursor.close()
     db.close()
-
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>"
-              .format(sys.argv[0]))
-        sys.exit(1)
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-list_cities(username, password, database)
