9001585a884dfcd4baad90dca3bf6d8f975bfe5b
kelvin <kelvin4stunt@gmail.com>
2023-11-20 10:09:07 +0100

    object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,36 +8,6 @@ import MySQLdb
 import sys
 
 
-def search_states(username, password, database, search_name):
-    """
-    Connects to the MySQL server and retrieves values
-    from the states table where the name matches the provided state name.
-
-    Args:
-        username (str): MySQL username.
-        password (str): MySQL password.
-        database (str): Database name.
-        search_name (str): State name to search for.
-
-    Returns:
-        None
-    """
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd="Mysqlpassword1$$",
-        db=database
-        )
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
-    states = cursor.fetchall()
-    for state in states:
-        print(state)
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
     if len(sys.argv) != 5:
         print("Usage: {} <username> <password> <database> <state_name>"
@@ -47,4 +17,19 @@ if __name__ == "__main__":
     password = sys.argv[2]
     database = sys.argv[3]
     search_name = sys.argv[4]
-search_states(username, password, database, search_name)
+
+    db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user=username,
+    passwd="Mysqlpassword1$$",
+    db=database
+    )
+    cursor =db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
+                   .format(search_name))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+    cursor.close()
+    db.close()
