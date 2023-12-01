49589feaf8a94f5c6d5e2c9188764e75d56f965f
kelvin <kelvin4stunt@gmail.com>
2023-12-01 22:36:57 +0100

    up

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" this script Writes a function that finds a
+    peak in a list of unsorted integers."""
+
+
+def find_peak(list_of_integers):
+    """
+    Find a peak in a list of unsorted integers.
+
+    Args:
+    - list_of_integers: List of unsorted integers.
+
+    Returns:
+    - Peak element.
+    """
+    if not list_of_integers:
+        return None
+
+    while low < high:
+        mid = (low + high) // 2
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
