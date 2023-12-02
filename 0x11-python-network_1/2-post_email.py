16cd78dd4c0414e795cd6fb8171b8f54e98478c1
kelvin <kelvin4stunt@gmail.com>
2023-12-02 11:53:07 +0100

    up

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Script to send a POST request with an email parameter to a URL and display the body of the response (decoded in utf-8).
+Script to send a POST request with an email parameter
+to a URL and display the body of the response (decoded in utf-8).
 """
 
 import urllib.request
@@ -12,7 +13,6 @@ if __name__ == "__main__":
     email = sys.argv[2]
     data = {'email': email}
     data = urllib.parse.urlencode(data).encode('utf-8')
-    
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url, data) as response:
