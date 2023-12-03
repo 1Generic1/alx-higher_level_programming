c81b30819426a34bf88dce0177ce9bba0e985558
kelvin <kelvin4stunt@gmail.com>
2023-12-03 10:58:07 +0100

    up

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Script to fetch https://alx-intranet.hbtn.io/status using the requests package.
+"""
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print("Body response:")
+        print("\t- type:", type(response.text))
+        print("\t- content:", response.text)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
