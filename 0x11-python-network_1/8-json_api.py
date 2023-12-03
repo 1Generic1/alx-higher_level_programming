4aa6fecbf0d8f17fe51e31f9638a171a1a3c6d94
kelvin <kelvin4stunt@gmail.com>
2023-12-03 17:53:18 +0100

    up

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script to send a POST request to http://0.0.0.0:5000/search_user
+with a letter as a parameter.
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    q = "" if len(sys.argv) < 2 else sys.argv[1]
+    try:
+        response = requests.post(
+            "http://0.0.0.0:5000/search_user",
+            data={'q': q}
+        )
+        json_response = (
+            response.json() if response.headers.get('content-type')
+            == 'application/json'
+            else None
+        )
+        if (
+            json_response and isinstance(json_response, dict) and
+            json_response.get('id') and json_response.get('name')
+        ):
+
+            print("[{}] {}".format(json_response['id'], json_response['name']))
+        else:
+            if not response.text:
+                print("No result")
+            else:
+                print("Not a valid JSON")
+    except ValueError:
+        print("Not a valid JSON")
