8bf9949cb20f25e6ef85984bc0fed203cd12ad77
kelvin <kelvin4stunt@gmail.com>
2023-12-03 12:35:00 +0100

    up

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,17 @@
+"""
+Script to send a POST request to a URL with an email parameter and display the body of the response.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'enail': email}
+    try:
+        response = requests.post(url, data=data)
+        response.raise_for_status()
+        print(response.text)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
