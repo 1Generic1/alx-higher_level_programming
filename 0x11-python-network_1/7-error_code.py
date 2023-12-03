2df9865484916e8a158f149b7b404048f55b1aa8
kelvin <kelvin4stunt@gmail.com>
2023-12-03 13:23:40 +0100

    up

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script to send a GET request to a URL and display the body of the response.
+If the HTTP status code is greater than or equal to 400, print an error message.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url =sys.argv[1]
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        print(response.text)
+        if response.status_code >= 40:
+            print("Error code:", response.status_code)
+    except requests.exceptions.RequestException as e:
+        print("Error:", e)
+
