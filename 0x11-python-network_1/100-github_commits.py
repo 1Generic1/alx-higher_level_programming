215786957e3fe1b95510561db5c054372fe8987e
kelvin <kelvin4stunt@gmail.com>
2023-12-04 14:58:49 +0100

    up

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Script to list 10 commits (from the most recent to oldest) of a repository
+by a specific user using the GitHub API.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
+    try:
+        url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
+        params = {"per_page": 10}
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            commits = response.json()
+            for commit in reversed(commits):
+                sha = commit["sha"]
+                author_name = commit["commit"]["author"]["name"]
+                print("{}: {}".format(sha, author_name))
+        else:
+            print("Error: {}".format(response.status_code))
+    except requests.exceptions.RequestException as e:
+        print("Error: {}".format(e))
