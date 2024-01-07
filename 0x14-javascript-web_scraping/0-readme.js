40f7f2d935b853b2c1fa761acc277b5b7979717f
kelvin <kelvin4stunt@gmail.com>
2024-01-07 17:24:41 +0100

    up

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,25 +2,21 @@
 
 const fs = require('fs');
 
-function readAndPrintFileContent(filePath)
-{
-	try {
-		const content = fs.readFileSync(filePath, 'utf-8');
-		console.log(content);
-	}
-	catch (error) {
-		if (error.code === 'EN0ENT') {
-			console.error(`File not found: ${error.path}`);
-		} else {
-			console.error(`An error occurred: ${error.message}`);
-		}
-	}
+function readAndPrintFileContent (filePath) {
+  try {
+    const content = fs.readFileSync(filePath, 'utf-8');
+    console.log(content);
+  } catch (error) {
+    if (error.code === 'EN0ENT') {
+      console.error(`File not found: ${error.path}`);
+    } else {
+      console.error(`An error occurred: ${error.message}`);
+    }
+  }
 }
-
 if (process.argv.length !== 3) {
-	console.error('Usage: ./readFile.js <file_path>');
-	process.exit(1);
+  console.error('Usage: ./readFile.js <file_path>');
+  process.exit(1);
 }
-
 const filePath = process.argv[2];
 readAndPrintFileContent(filePath);
