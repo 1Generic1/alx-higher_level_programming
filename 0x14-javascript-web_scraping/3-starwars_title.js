9dba9f4c8e5626707aa4d9b5ce6df34eab0fbbc0
kelvin <kelvin4stunt@gmail.com>
2024-01-08 10:58:24 +0100

    up

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -12,15 +12,15 @@ const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(apiUrl, (error, response, body) => {
   if (error) {
-    console.error(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`);
   } else if (response.statusCode !== 200) {
-    console.error(`Unexpected HTTP status code: ${response.statusCode}`);
+    console.log(`Unexpected HTTP status code: ${response.statusCode}`);
   } else {
     try {
       const movieData = JSON.parse(body);
       console.log(`Title: ${movieData.title}`);
     } catch (parseError) {
-      console.error(`Error parsing JSON response: ${parseError.message}`);
+      console.log(`Error parsing JSON response: ${parseError.message}`);
     }
   }
 });
