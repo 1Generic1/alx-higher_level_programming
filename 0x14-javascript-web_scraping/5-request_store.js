15de21279a8d57779f462b4f5ccee799ad52cfc5
kelvin <kelvin4stunt@gmail.com>
2024-01-09 09:25:55 +0100

    up

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: ./saveWebpage.js <url> <file_path>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+  } else if (response.statusCode !== 200) {
+    console.error(`Unexpected HTTP status code: ${response.statusCode}`);
+  } else {
+    fs.writeFileSync(filePath, body, 'utf-8');
+    console.log(`Webpage content saved to: ${filePath}`);
+  }
+});
